c30827f8fa0597bfa6410813882d8bfc7e57fa50
oktonion <0oktonion0@gmail.com>
2022-06-16 17:50:25 +0300

    1

---
+++ ImGuiStudio/include/ImGuiStudio.h
@@ -0,0 +1,24 @@
+#ifndef _IMGUI_STUDIO_H
+#define _IMGUI_STUDIO_H
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+// ImGui Studio includes
+/*none*/
+
+// third-party includes
+/*none*/
+
+// std includes
+/*none*/
+
+namespace ImGuiStudio
+{
+
+
+} // namespace ImGuiStudio
+
+
+#endif // _IMGUI_STUDIO_H
